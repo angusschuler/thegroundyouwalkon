887ba1f42d0cd1c58b8ddc7b3526bc3b223b44fc
Angus Schuler <asc132@uclive.ac.nz>
2019-10-18 01:05:06 +1300

    Updates

---
+++ precache-manifest.9a14fabc3c60ea49d925b5d4adde1799.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5beb7b9a80de3d9172ed07f66cec67b9",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "3e85f9f6948edcf00260",
+    "url": "/myapp/static/css/main.6e61883d.chunk.css"
+  },
+  {
+    "revision": "0963b2e455d79fad143a",
+    "url": "/myapp/static/js/2.2a1e97cc.chunk.js"
+  },
+  {
+    "revision": "3e85f9f6948edcf00260",
+    "url": "/myapp/static/js/main.4b2a18f8.chunk.js"
+  },
+  {
+    "revision": "ccb5ae81495d44705560",
+    "url": "/myapp/static/js/runtime~main.5817b5c5.js"
+  }
+]);
